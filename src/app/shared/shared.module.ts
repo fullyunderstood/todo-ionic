44f99a78bf025fe6901414b6faf7a27dee478665
coder-shanks <coder.shanks@gmail.com>
2020-04-12 00:54:32 +0530

    feat: created component to show custom text for selected date

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { CommonModule, DatePipe } from '@angular/common';
+import { DateModifyComponent } from './components/date-modify/date-modify.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DateModifyComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [
+    DateModifyComponent
+  ],
+  providers: [
+    DatePipe
+  ]
 })
 export class SharedModule { }
